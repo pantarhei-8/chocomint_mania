7967aa946e32153be660cbeb5aa6e5c1f6c89c71
pantarhei-8 <69030976+pantarhei-8@users.noreply.github.com>
2020-10-06 11:34:20 +0900

    rails g 自動生成ファイルの設定
    gem 'pry-rails' 導入

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,16 @@ module ChocomintMania
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    config.generators do |g|
+      g.assets false
+      g.skip_routes true
+      g.test_framework :rspec,    # RSpecを使用
+        controller_specs: false,  # controller specは作らない
+        view_specs: false,        # view specは作らない
+        helper_specs: false,      # helper specは作らない
+        routing_specs: false      # routing specは作らない
+    end
+
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
