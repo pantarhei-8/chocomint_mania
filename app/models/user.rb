bbcd4cb557afc1fa58eba2cd6cd52f8defba10b4
pantarhei-8 <69030976+pantarhei-8@users.noreply.github.com>
2020-10-07 15:40:19 +0900

    userモデルのバリデーション記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,17 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, password_length: 6..12
+
+#バリデーション
+validates :name, presence: true, length: { maximum: 20 }
+validates :name, uniqueness: true
+
+VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+validates :email, {presence: true, ,
+                  format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }}
+
+VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+validates :password, presence: true, format: { with: VALID_PASSWORD_REGEX }
+
 end
