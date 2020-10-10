344bb61e83bb48761c298f0319632b78b323808f
pantarhei-8 <69030976+pantarhei-8@users.noreply.github.com>
2020-10-10 13:22:26 +0900

    userモデル　バリデーション修正
    db:rollbackした後、再度migrate

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ validates :name, presence: true, length: { maximum: 20 }
 validates :name, uniqueness: true
 
 VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-validates :email, {presence: true, ,
+validates :email, {presence: true,
                   format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }}
 
 VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
