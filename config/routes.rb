790af674f67780afb8e1adac5a2d8a4cd979fe70
pantarhei-8 <69030976+pantarhei-8@users.noreply.github.com>
2020-10-11 16:35:17 +0900

    アカウント作成画面
    CSSは保留。

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root 'tops#index'
   get :terms_of_service, to: 'tops#terms'
-  resources :users, only: [:index, :show, :edit, :udate]
+  resources :users, only: [:index, :show, :edit, :update]
 end
