dc5f042c9ee7b46acda2f845846b71f9b3b168bd
pantarhei-8 <69030976+pantarhei-8@users.noreply.github.com>
2020-10-11 22:55:44 +0900

    showアクション　viewの設置

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,20 @@
 class UsersController < ApplicationController
+  skip_before_action :authenticate_user!, only: :show
+  
+  
   def show
+    @user = User.find(params[:id])
   end
 
   def update
   end
+
+
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :image, :introduction)
+    end
+
+
 end
