859a7a2d9c2401904ff654cadb6a28db935bd857
pantarhei-8 <69030976+pantarhei-8@users.noreply.github.com>
2020-10-13 10:20:05 +0900

    Userモデルの単体テスト　記述

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :user do
-    
+    name                    {Faker::Name.initials}
+    email                   {Faker::Internet.free_email}
+    password                {"123abc"}
+    password_confirmation   {password}
+    introduction            {"よろしくお願いいたします。"}
   end
 end
